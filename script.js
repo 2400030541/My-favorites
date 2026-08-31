461949f6f454f267df4704a83b55641aa7886b14
2400030541 <2400030541@kluniversity.in>
2026-08-31 18:18:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = task + 
+        ' <button onclick="this.parentElement.remove()">❌</button>';
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
